9d05dcc37624f5993c7042be3aa2c72afb5774fb
spy21470 <108908547+spy21470@users.noreply.github.com>
2026-03-02 19:24:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -94,6 +94,11 @@ app.post("/update", async (req, res) => {
     res.redirect("/");
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 app.listen(3000, () => {
     console.log("Server started on http://localhost:3000");
 });
